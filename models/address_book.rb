00137fa9ed0acfb17c833ad31ab518b82405526e
Forrest Stone <iloveburningman@gmail.com>
2016-05-15 16:52:17 -0700

    messed up and was fixing it

--- models/address_book.rb
+++ models/address_book.rb
@@ -31,6 +31,13 @@ class AddressBook
      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
    end
   end
+
+  # Search AddressBook for a specific entry by name
+   def binary_search(name)
+
+   end
+
+
   #past the follow along code
   def remove_entry(name, phone_number, email)
     entries.each_with_index do |entry, index|
